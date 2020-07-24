5ea1c7e4253f187de260a40e0123d9c279187694
MaryamMosstoufi <maryam@zooxoos.com>
2020-07-24 11:33:23 -0400

    Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,33 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Login from "./components/Login";
-import "./styles.scss";
+//import "./styles.scss";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <div>
+          <div className='uk-margin-bottom'>
+            <nav className="uk-navbar-container" data-uk-navbar>
+              <div className="uk-navbar-center">
+                <ul className="uk-navbar-nav">
+                  <li><Link to='/login'>Login</Link></li>
+                  <li><Link to='/bubbles'>Bubbles</Link></li>
+                  <li><Link to='/addcolor'>Add Color</Link></li>
+                </ul>
+              </div>
+            </nav>
+          </div>  
+        </div>
+        <div className='uk-container uk-flex uk-flex-center'>
+          <Route exact path="/" component={Login} />
+        </div>
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,65 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios';
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+class Login extends React.Component {
+  state = {
+    credentials: {
+      username: '',
+      password: ''
+    }
+  };
 
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+  login = e => {
+    e.preventDefault();
+    axios.post("http://localhost:5000/api/login", this.state.credentials)
+      .then(res => {
+        console.log(res);
+        localStorage.setItem("token", res.data.payload);
+        this.props.history.push("/bubbles");
+      })
+      .catch(err => console.log({ err }))
+  }
+  render() {
+    return (
+      <div className='uk-width-medium uk-card uk-card-default uk-card-body'>
+        <form onSubmit={this.login}>
+          <fieldset className='uk-fieldset'>
+
+            <legend className='uk-legend uk-text-center'>Login</legend>
+
+            <div className='uk-margin'>
+              <input
+                className='uk-input'
+                type='text'
+                name='username'
+                onChange={this.handleChange}
+                value={this.state.credentials.username}
+                placeholder='Username' />
+            </div>
+            <div className='uk-margin'>
+              <input
+                className='uk-input'
+                type='password'
+                name='password'
+                onChange={this.handleChange}
+                value={this.state.credentials.password}
+                placeholder='Password' />
+            </div>
+            <div className='uk-margin'>
+              <button className='uk-button uk-button-secondary uk-width-1-1'>Login</button>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    )
+  }
+}
 export default Login;
